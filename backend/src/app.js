9d43b6d988327e4e9017ab987200240dcab1233c
Amit <amitcoder07@gmail.com>
2026-08-07 22:01:13 +0530

    payment api completed

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import workerRoutes from "./routes/worker.routes.js";
 import jobRoutes from "./routes/job.routes.js";
 import applicationRoutes from "./routes/application.routes.js";
 import reviewRoutes from "./routes/review.routes.js";
+import paymentRoutes from "./routes/payment.routes.js";
 const app = express();
 
 app.use(express.json());
@@ -13,4 +14,5 @@ app.use("/api/worker", workerRoutes);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/applications", applicationRoutes);
 app.use("/api/reviews", reviewRoutes);
+app.use("/api/payments", paymentRoutes);
 export default app;
